75767480acd798205945d84d8582c99133d65314
Dmitriy Danilin <danilindn@icloud.com>
2020-11-17 17:06:23 +0300

    Окончание урока 11

--- MyPlaces/NewPlaceTableViewController.swift
+++ MyPlaces/NewPlaceTableViewController.swift
@@ -17,7 +17,26 @@ class NewPlaceTableViewController: UITableViewController {
     // MARK: - Table view delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+            // Создаём алерт контроллер (выползающее меню снизу) для добавления фото
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
             
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            // Добавляем в контроллер созданные экшены
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            // Вызываем созданный контроллер
+            present(actionSheet, animated: true)
         } else {
             view.endEditing(true)
             // Отключаем разлиновку TableVIew ниже имеющихся ячеек
@@ -34,5 +53,20 @@ extension NewPlaceTableViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+}
+
+// MARK: - Работа с изображениями
+extension NewPlaceTableViewController {
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        // Проверяем доступность источника выбора изображений
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            // Включаем возможность редактировать выбранное изображение
+            imagePicker.allowsEditing = true
+            // Определяем тип источника выбранного изображения
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
     }
 }
