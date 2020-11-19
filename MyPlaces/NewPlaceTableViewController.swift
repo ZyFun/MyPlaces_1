5d917356a931078f23146a87da9e5a4c0ae24248
Dmitriy Danilin <danilindn@icloud.com>
2020-11-19 15:18:00 +0300

    Окончание уроков 12 и 13

--- MyPlaces/NewPlaceTableViewController.swift
+++ MyPlaces/NewPlaceTableViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class NewPlaceTableViewController: UITableViewController {
 
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +19,8 @@ class NewPlaceTableViewController: UITableViewController {
     // MARK: - Table view delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+            let cameraIcon = #imageLiteral(resourceName: "camera")
+            let photoIcon = #imageLiteral(resourceName: "photo")
             // Создаём алерт контроллер (выползающее меню снизу) для добавления фото
             let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
             
@@ -24,9 +28,19 @@ class NewPlaceTableViewController: UITableViewController {
                 self.chooseImagePicker(source: .camera)
             }
             
+            // Устанавливаем иконку для экземпляра контроллера
+            camera.setValue(cameraIcon, forKey: "image")
+            // Смещаем текст к левой границе
+            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
             let photo = UIAlertAction(title: "Photo", style: .default) { _ in
                 self.chooseImagePicker(source: .photoLibrary)
             }
+            
+            // Устанавливаем иконку для экземпляра контроллера
+            photo.setValue(photoIcon, forKey: "image")
+            // Смещаем текст к левой границе
+            photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
             
             let cancel = UIAlertAction(title: "Cancel", style: .cancel)
             
@@ -57,16 +71,30 @@ extension NewPlaceTableViewController: UITextFieldDelegate {
 }
 
 // MARK: - Работа с изображениями
-extension NewPlaceTableViewController {
+extension NewPlaceTableViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func chooseImagePicker(source: UIImagePickerController.SourceType) {
         // Проверяем доступность источника выбора изображений
         if UIImagePickerController.isSourceTypeAvailable(source) {
             let imagePicker = UIImagePickerController()
+            // Назначаем imagePicker делегатом функции imagePickerController. Протокол: UINavigationControllerDelegate
+            imagePicker.delegate = self
             // Включаем возможность редактировать выбранное изображение
             imagePicker.allowsEditing = true
             // Определяем тип источника выбранного изображения
             imagePicker.sourceType = source
             present(imagePicker, animated: true)
         }
+    }
+    
+    // Добавляем метод, позваляющий добавить выбранное изображение протокол: UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Присваиваем свойство отредактированного изображения
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        // Масштабируем изображение по содержимому
+        imageOfPlace.contentMode = .scaleAspectFill
+        // Обрезаем по границе изображения
+        imageOfPlace.clipsToBounds = true
+        // Закрываем контроллер
+        dismiss(animated: true)
     }
 }
